73a16e7d2ca0f0b928af3031a63f52382e12eec7
G4M3R-Mike\Michael <michaelp.gudz@gmail.com>
2022-10-30 01:51:16 +0200

    Created a new CartSummary ViewComponent

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart { get; }
+        public CartSummaryViewComponent(Cart cartSerivce)
+        {
+            cart = cartSerivce;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+
+    }
+}
